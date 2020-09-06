5e132ca2814167b47d529b301def23af1466a800
WEB RATTNA <69116458+NantawatTOMOHA@users.noreply.github.com>
2020-09-06 20:13:06 +0700

    Add files via upload

---
+++ NUMBER 6.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+void loop(int);
+
+int main() {
+	int f;
+	cout << "ENTER NUMBER 6 : ";
+	cin >> f;
+		if (f==6) {
+			int x;
+			for (x = 1; x <= 10001; x++) {
+
+				loop(x);
+			}
+		}
+	return 0;
+}
+
+void loop(int x) {
+	struct koop {
+		int x[2];
+		int ansplus;
+	};
+	koop L;
+	L.x[1] = 1;
+	L.x[0] = x;
+	L.ansplus = 0;
+	for (L.x[1] = 1; L.x[1] <= L.x[0]; L.x[1]++)
+	{
+
+
+		if ((L.x[0] % L.x[1]) == 0 && L.x[0] != L.x[1])
+		{
+
+			L.ansplus = L.ansplus + L.x[1];
+
+		}
+		else if (L.ansplus == L.x[0]) {
+			cout <<"NUMBER P(x)=x :"<< L.ansplus << endl;
+			L.ansplus = 0;
+			break;
+		}
+		else if (L.ansplus != L.x[0] && L.x[0] == L.x[1]) {
+			L.ansplus = 0;
+		}
+		else {}
+
+	}
+}
